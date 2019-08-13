cc97e9707b2a7472af28aa8b4dd3e2c26b7115c1
Rahul255 <rahulmohan255@gmail.com>
2019-08-13 16:35:17 +0530

    file added

--- mb/posts/views.py
+++ mb/posts/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Post
 # Create your views here.
+
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
